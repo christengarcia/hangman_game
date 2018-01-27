9f42f35451e9f031999cee51815eccaf18110506
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-28 10:23:12 +1100

    Initial commit

---
+++ hangman_game/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ *  C# Program to Create a HangMan Game
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hangman_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Hangman!!!!!!!!!!");
+            string[] listwords = new string[10];
+            listwords[0] = "sheep";
+            listwords[1] = "goat";
+            listwords[2] = "computer";
+            listwords[3] = "america";
+            listwords[4] = "watermelon";
+            listwords[5] = "icecream";
+            listwords[6] = "jasmine";
+            listwords[7] = "pineapple";
+            listwords[8] = "orange";
+            listwords[9] = "mango";
+            Random randGen = new Random();
+            var idx = randGen.Next(0, 9);
+            string mysteryWord = listwords[idx];
+            char[] guess = new char[mysteryWord.Length];
+            Console.Write("Please enter your guess: ");
+        }
+    }
+}
